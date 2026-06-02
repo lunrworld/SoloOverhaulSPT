0c835bb23dad90ccb6138474869818cac0f5ac45
lunrworld <lunrworld@gmail.com>
2026-06-02 12:46:02 -0500

    .

--- Server/Services/ConfigService.cs
+++ Server/Services/ConfigService.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Utils;
+
 
 namespace SoloOverhaul.Services
 {
-    internal class ConfigService
+    [Injectable(InjectionType.Singleton)]
+    internal class ConfigService(ModHelper modHelper, JsonUtil jsonUtil, ISptLogger<ConfigService> logger)
     {
+        public SOHConfig SOHConfig { get; private set; } = new();
+
+        public string GetModPath()
+        {
+            return modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        }
+
+        public string GetConfigPath()
+        {
+            return Path.Combine(GetModPath(), "Config", "config.json");
+        }
+
     }
 }
